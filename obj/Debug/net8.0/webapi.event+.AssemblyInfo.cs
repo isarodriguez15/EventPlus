5c1e2f95dc9fd9146b24115b4d7358d262bf1313
Isabelle Rodrigues <isabelle.r.reis@aluno.senai.br>
2025-04-01 16:46:23 -0300

    feat: adicionando implementacoes

--- obj/Debug/net8.0/webapi.event+.AssemblyInfo.cs
+++ obj/Debug/net8.0/webapi.event+.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webapi.event+")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de07c9e05f02eef2b7511147dcd5c00026db97fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a679a9b5dd5eb29b689651c1beec3e76720c9508")]
 [assembly: System.Reflection.AssemblyProductAttribute("webapi.event+")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webapi.event+")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
